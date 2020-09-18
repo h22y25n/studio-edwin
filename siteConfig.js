7c262f09e5e4bf9f47a552697cd7654cc1a9909a
강희연 <k.h22y25n@gmail.com>
2020-09-18 16:11:27 +0900

    init project

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Studio Edwin',
+  shortName: '',
+  description: 'Studio Edwin based Seoul, Korea',
+  url: 'https://www.jooplaan.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'edwin',
+  twitter: ''
+}
